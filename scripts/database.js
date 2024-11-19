ec05c1a71ba128efeafada06187abae95a955b07
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2024-11-19 16:08:21 -0300

    16:10

--- scripts/database.js
+++ scripts/database.js
@@ -1,9 +1,9 @@
 export const database = [ /* "Banco de dados" de produtos comercializados no site */
     {
         codigoProduto:1,
-        tituloProduto:"Furadeira e Parafusadeira de Impacto a Bateria Bosch com Carregador e Bateria 18V 1/2 GSB 180-Li Bivolt", 
+        tituloProduto:"Nike Undefeated x Kobe 4 Protro 'Court Purple'", 
         preco: 3200.00, 
-        descricao:"Para furar madeiras, metais e alvenaria, a Furadeira e Parafusadeira GSB 180-Li é a solução ideal! Com o maior torque da categoria, baixa manutenção e a maior resistência se comparar com os principais concorrentes, é sinônimo de máxima performance com o melhor custo benefício.",
+        descricao:"O Undefeated x Kobe 4 Protro 'Kyle Kuzma' vem em uma colorway inspirada no Los Angeles Lakers, a equipe que contratou o Kuzma em 2017 e onde Kobe Bryant passou toda a sua carreira de 20 anos na NBA. O sapato é uma das quatro colorways lançadas em agosto de 2019 e apresenta detalhes da assinatura Kobe, marca Undefeated e uma parte superior de couro roxo. ",
         imagemProduto: {
             img1:"./images/products/prod01/img01.png",
             img2:"./images/products/prod01/img2.png",
@@ -11,40 +11,40 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
             img4:"./images/products/prod01/img4.png",
 
         },
-        categoriaProduto: 'Ferramentas',
+        categoriaProduto: 'Sneakers',
         classificacaoProduto:"Novidades",
         exibirHome: true
     },
     {
         codigoProduto:2,
-        tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
-        preco: 300.00, 
-        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+        tituloProduto:"Nike Zoom Kobe 4 Gold Medal", 
+        preco: 1686.99, 
+        descricao:"Inspirado na corrida redentora da equipe em Pequim, esta edição do Kobe 4 Protro retorna com o mesmo espírito vencedor e elogios para provar isso. Uma mistura brilhante de ouro metálico e cinza neutro veste a parte superior leve e em camadas, enquanto detalhes em obsidiana escura e acabamentos de alto brilho adicionam um brilho merecido. ",
         imagemProduto: {
             img1:"./images/products/prod02/img1.png",
             img2:"./images/products/prod02/img2.png",
             img3:"./images/products/prod02/img3.png",
             img4:"./images/products/prod02/img4.png",
     },
-        categoriaProduto: 'Ferramentas',
+        categoriaProduto: 'Sneakers',
         classificacaoProduto:"Novidades",
         exibirHome: true
     },
     {
         codigoProduto:3,
-        tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
-        preco: 300.00, 
-        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+        tituloProduto:"Kobe IV Protro FTB", 
+        preco: 3589.99, 
+        descricao:"Como seu apelido sugere, o Zoom Kobe 4 Protro 'FTB' se inspira na coleção 2016 da Nike 'Fade to Black', que celebrou a aposentadoria de Kobe Bryant com treze de seus tênis em cores monocromáticas que passaram do branco ao preto. Este par apresenta um padrão de pele de cobra em relevo no antepé e na gola de camurça do tênis, ao lado de detalhes em ouro e azul-petróleo em toda a parte superior.",
         imagemProduto: {
-            img1:"./images/products/prod02/img1.png",
-            img2:"./images/products/prod02/img2.png",
-            img3:"./images/products/prod02/img3.png",
-            img4:"./images/products/prod02/img4.png",
+            img1:"./images/products/prod03/img1.png",
+            img2:"./images/products/prod03/img2.png",
+            img3:"./images/products/prod03/img3.png",
+            img4:"./images/products/prod03/img4.png",
 
         },
-        categoriaProduto: 'Ferramentas',
+        categoriaProduto: 'Sneakers',
         classificacaoProduto:"Novidades",
-        exibirHome: false
+        exibirHome: true
     },
     {
         codigoProduto:4,
@@ -52,10 +52,10 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
         preco: 300.00, 
         descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
         imagemProduto: {
-            img1:"./images/products/prod02/img1.png",
-            img2:"./images/products/prod02/img2.png",
-            img3:"./images/products/prod02/img3.png",
-            img4:"./images/products/prod02/img4.png",
+            img1:"./images/products/prod04/img1.png",
+            img2:"./images/products/prod04/img2.png",
+            img3:"./images/products/prod04/img3.png",
+            img4:"./images/products/prod04/img4.png",
 
         },
         categoriaProduto: 'Ferramentas',
