9e449870c32879ee708fada550983a3542b04a4e
Victor Renato de Souza <53796509886@>
2024-11-26 16:58:12 -0300

    16:58

--- scripts/database.js
+++ scripts/database.js
@@ -48,9 +48,9 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
     },
     {
         codigoProduto:4,
-        tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
-        preco: 300.00, 
-        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+        tituloProduto:"Tênis Nike Zoom Kobe 4 Protro 'Gigi'", 
+        preco: 2900.99, 
+        descricao:"Conheça o Tênis Nike Zoom Kobe 4 Protro 'Mambacita', um modelo que homenageia a filha do lendário jogador de basquete Kobe Bryant. Este tênis é muito mais do que um calçado, é uma declaração de estilo e respeito a um dos maiores atletas da história.",
         imagemProduto: {
             img1:"./images/products/prod04/img1.png",
             img2:"./images/products/prod04/img2.png",
@@ -58,88 +58,73 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
             img4:"./images/products/prod04/img4.png",
 
         },
-        categoriaProduto: 'Ferramentas',
-        classificacaoProduto:"Promocoes",
+        categoriaProduto: 'Sneakers',
+        classificacaoProduto:"Novidades",
         exibirHome: true
-    },
+    }, 
     {
         codigoProduto:5,
-        tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
-        preco: 300.00, 
-        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+        tituloProduto:"Kobe 5 ProtroDeep Royal Blue", 
+        preco: 1499.99, 
+        descricao:"O Kobe 5 Protro é equipado com uma unidade Air Zoom flexível e tração reduzida para uma sensação ideal na quadra. Além disso, o cabedal minimamente estruturado é leve e oferece suporte em todos os aspectos.",
         imagemProduto: {
-            img1:"./images/products/prod02/img1.png",
-            img2:"./images/products/prod02/img2.png",
-            img3:"./images/products/prod02/img3.png",
-            img4:"./images/products/prod02/img4.png",
+            img1:"./images/products/prod05/img1.png",
+            img2:"./images/products/prod05/img2.png",
+            img3:"./images/products/prod05/img3.png",
+            img4:"./images/products/prod05/img4.png",
 
         },
-        categoriaProduto: 'Ferramentas',
+        categoriaProduto: 'Sneakers',
         classificacaoProduto:"Mais_Vendidos",
         exibirHome: true
     },
     {
         codigoProduto:6,
-        tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
-        preco: 300.00, 
-        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+        tituloProduto:"Kobe V ProtroDel Sol/Metallic Silver", 
+        preco: 2789.00, 
+        descricao:"Uma sensação em seu primeiro lançamento, o Kobe V deu início a uma nova era de canos baixos na liga. A agilidade do tênis combinada com sua passada próxima do chão proporcionaram uma plataforma para o primeiro passo devastador do Kobe, ajudando-o a criar espaço e a arremessar de qualquer lugar na quadra. O Kobe V Protro ressalta o original com amortecimento aprimorado, melhor tração e sensação de quadra.",
         imagemProduto: {
-            img1:"./images/products/prod02/img1.png",
-            img2:"./images/products/prod02/img2.png",
-            img3:"./images/products/prod02/img3.png",
-            img4:"./images/products/prod02/img4.png",
+            img1:"./images/products/prod06/img1.png",
+            img2:"./images/products/prod06/img2.png",
+            img3:"./images/products/prod06/img3.png",
+            img4:"./images/products/prod06/img4.png",
 
         },
-        categoriaProduto: 'Ferramentas',
+        categoriaProduto: 'Sneakers',
         classificacaoProduto:"Mais_Vendidos",
         exibirHome: true
     },
     {
         codigoProduto:7,
-        tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
-        preco: 300.00, 
-        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+        tituloProduto:"Kobe 8 Protro", 
+        preco: 1199.00, 
+        descricao:"Kobe é o único jogador na história da NBA a ter dois números aposentados no mesmo time. Este Kobe 8 Protro celebra seu feito incomparável - e sua ética de trabalho incomparável - em uma colorway Court Purple e White, além de toques de design exclusivos para a marca inconfundível do Mamba. A malha leve e arejada na parte superior e uma placa arqueada no meio do pé acrescentam conforto, suporte e estabilidade que tornam este ícone um pilar em todos os níveis de jogo",
         imagemProduto: {
-            img1:"./images/products/prod02/img1.png",
-            img2:"./images/products/prod02/img2.png",
-            img3:"./images/products/prod02/img3.png",
-            img4:"./images/products/prod02/img4.png",
+            img1:"./images/products/prod07/img1.png",
+            img2:"./images/products/prod07/img2.png",
+            img3:"./images/products/prod07/img3.png",
+            img4:"./images/products/prod07/img4.png",
 
         },
-        categoriaProduto: 'Ferramentas',
+        categoriaProduto: 'Sneakers',
         classificacaoProduto:"Mais_Vendidos",
         exibirHome: true
     },
     {
         codigoProduto:8,
-        tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
-        preco: 300.00, 
-        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+        tituloProduto:"Kobe 6 Reverse Grinch", 
+        preco: 4500.00, 
+        descricao:"Este Kobe 6 Protro pega uma colorway cobiçada de 2010 e a inverte. Elegante e escamoso, o cabedal Bright Crimson é ousado com destaques em Electric Green e um padrão de cobra inspirado no apelido de Black Mamba de Kobe. Ele está equipado com uma unidade Zoom Air flexível e espuma responsiva – e com um padrão de tração reduzido, você obtém um calçado mais leve",
         imagemProduto: {
-            img1:"./images/products/prod02/img1.png",
-            img2:"./images/products/prod02/img2.png",
-            img3:"./images/products/prod02/img3.png",
-            img4:"./images/products/prod02/img4.png",
+            img1:"./images/products/prod08/img1.png",
+            img2:"./images/products/prod08/img2.png",
+            img3:"./images/products/prod08/img3.png",
+            img4:"./images/products/prod08/img4.png",
 
         },
-        categoriaProduto: 'Ferramentas',
+        categoriaProduto: 'Sneakers',
         classificacaoProduto:"Mais_Vendidos",
         exibirHome: true
     },
-    {
-        codigoProduto:9,
-        tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
-        preco: 300.00, 
-        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
-        imagemProduto: {
-            img1:"./images/products/prod02/img1.png",
-            img2:"./images/products/prod02/img2.png",
-            img3:"./images/products/prod02/img3.png",
-            img4:"./images/products/prod02/img4.png",
 
-        },
-        categoriaProduto: 'Ferramentas',
-        classificacaoProduto:"Mais_Vendidos",
-        exibirHome: true
-    },
 ]
